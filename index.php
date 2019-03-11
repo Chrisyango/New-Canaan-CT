15dfdfcee2251d8aab9eae1c5386346b11ab5ef2
Chris Yang <cyang287@yahoo.com>
2019-03-11 10:44:50 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>New Canaan, CT</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/css/animate.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="_assets_/css/jquery.mb.YTPlayer.min.css" media="all" type="text/css">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header>
+	<div class="container">
+		<a href="./" class="logo">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div id="nav-toggle" class="fa fa-bars"></div>
+		</div><!-- /#toggles -->
+
+		<div id="top-nav">
+			<div class="weather hidden-sm hidden-xs">
+				<div id="weather"></div><!-- /#weather -->
+			</div><!-- /.weather -->
+
+			<div class="social-links">
+				<a href="./" class="fa fa-twitter"></a>
+				<a href="./" class="fa fa-facebook"></a>
+			</div><!-- /.social-links -->
+
+			<div id="search">
+				<form class="search-form" method="get" action="search.php">
+					<label class="fa fa-search" for="search-input"></label>
+					<input name="q" class="form-control search-input" placeholder="Enter search terms..." type="search" id="search-input">
+					<button>search<i class="fa fa-search"></i></button>
+				</form><!-- /.search-form -->
+			</div><!-- /#search -->
+		</div><!-- /#top-nav -->
+
+		<nav>
+			<ul id="nav">
+				<li><a href="./">community</a></li>
+				<li><a href="./">government</a></li>
+				<li><a href="./">departments</a></li>
+				<li><a href="./">services</a></li>
+				<li><a href="./">schools</a></li>
+				<li><a href="./">how do i...</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+	</div><!-- /.container -->
+</header>
+
+<main id="main">
+	<section id="video-wrap" style="background: url('./_assets_/images/hero.jpg') center no-repeat; background-size: cover;">
+		<div id="video-bg" class="player"
+			data-property="{videoURL:'https://www.youtube.com/watch?v=Ghn-B3jmuSA',containment:'#video-bg',startAt:0,mute:false,autoPlay:false,loop:true,opacity:1,showControls:false}">
+		</div>
+		<div id="hero-whitespace" style="background: url('./_assets_/images/hero-whitespace.png') center no-repeat; background-size: cover;"></div>
+	</section><!-- /#slider -->
+
+	<section id="quick-links">
+		<div class="container">
+			<a href="./" class="quick-link">
+				<div style="background:url('./_assets_/images/quick-link1.png') center no-repeat;background-size:cover;"></div>
+				recreation
+			</a><!-- /.quick-links -->
+			<a href="./" class="quick-link">
+				<div style="background:url('./_assets_/images/quick-link2.png') center no-repeat;background-size:cover;"></div>
+				online bill pay
+			</a><!-- /.quick-links -->
+			<a href="./" class="quick-link">
+				<div style="background:url('./_assets_/images/quick-link3.png') center no-repeat;background-size:cover;"></div>
+				agendas + minutes
+			</a><!-- /.quick-links -->
+			<a href="./" class="quick-link">
+				<div style="background:url('./_assets_/images/quick-link4.png') center no-repeat;background-size:cover;"></div>
+				forms + documents
+			</a><!-- /.quick-links -->
+			<a href="./" class="quick-link">
+				<div style="background:url('./_assets_/images/quick-link5.png') center no-repeat;background-size:cover;"></div>
+				job opportunities
+			</a><!-- /.quick-links -->
+			<a href="./" class="quick-link">
+				<div style="background:url('./_assets_/images/quick-link6.png') center no-repeat;background-size:cover;"></div>
+				interactive maps
+			</a><!-- /.quick-links -->
+		</div><!-- /.container -->
+	</section><!-- /#quick-links -->
+
+	<section id="content">
+		<div class="container">
+			<section id="news">
+				<h2>news stories</h2>
+				<a href="./" class="btn">news archives</a>
+				<div id="news-links" class="owl-carousel">
+					<div class="news-link">
+						<span>november 22, 2018</span>
+						<h3>new canaan article and story description 1</h3>
+						<a href="./">read more</a>
+					</div><!-- /.news-links -->
+					<div class="news-link">
+						<span>november 22, 2018</span>
+						<h3>new canaan article and story description 1</h3>
+						<a href="./">read more</a>
+					</div><!-- /.news-links -->
+				</div><!-- /#news-links -->
+			</section><!-- /#news -->
+
+			<section id="notification">
+				<h2>notification sign-up</h2>
+				
+				<div id="signup">
+					<form>
+						<label for="signup-input">Sign-up for Notifications and Local Alerts</label>
+						<input name="q" class="form-control" placeholder="enter email address..." id="signup-input">
+						<button><span>submit</span> <span>register</span></button>
+						<button>manage</button>
+					</form><!-- /.search-form -->
+				</div><!-- /#search -->
+			</section>
+
+			<section id="calendar">
+				<img src="./_assets_/images/calendar.png" alt="Calendar">
+			</section>
+		</div><!-- /.container -->
+	</section><!-- /#content -->
+
+	<section id="connect">
+		<div class="container">
+			<h2>connect with us</h2>
+			<div>
+				<a href="./">@townofnewcanaan</a>
+				<ul id="tabs">
+					<li class="active"><a href="./" class="fa fa-facebook"></a></li>
+					<li><a href="./" class="fa fa-twitter"></a></li>
+				</ul><!-- /#tabs -->
+			</div>
+			<div id="social-wrapper">
+				<div id="tabs-content">
+					<div class="active">
+						<div id="social-feed"></div>
+					</div>
+					<div>
+						twitter stuffs
+					</div>
+				</div>
+			</div><!-- /#social-wrapper -->
+		</div><!-- /.container -->
+	</section><!-- /#connect -->
+
+	<section id="footer-links">
+		<div class="container">
+			<section id="left-links">
+				<div id="footer-nav">
+					<a href="./">community</a>
+					<a href="./">goverment</a>
+					<a href="./">departments</a>
+					<a href="./">services</a>
+					<a href="./">how do i...</a>
+				</div><!-- /#footer-nav -->
+				<div id="contact-info">
+					<span>&copy; 2018 new canaan</span>
+					<span>77 main street, new canaan, ct 06840</span>
+					<span>(203) 594-3054</span>
+				</div><!-- /#contact-info -->
+			</section><!-- /#left-links -->
+			<section id="right-links">
+				<a href="./">staff directory</a>
+				<div class="social-links">
+					<a href="./" class="fa fa-twitter"></a>
+					<a href="./" class="fa fa-facebook"></a>
+				</div><!-- /.social-links -->
+				<a href="./">login</a>
+			</section><!-- /#right-links -->
+		</div><!-- /.container -->
+	</section><!-- /#footer-links -->
+</main>
+
+<footer>
+	<div class="container">
+			<span>Powered by <a href="https://www.revize.com/" id="revize-logo">revize.</a>, The Government Website Experts</span>
+	</div><!-- /.container -->
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/social-feed/bower_components/codebird-js/codebird.js"></script>
+<script src="_assets_/plugins/social-feed/bower_components/doT/doT.min.js"></script>
+<script src="_assets_/plugins/social-feed/bower_components/moment/min/moment.min.js"></script>
+<script src="_assets_/plugins/social-feed/bower_components/moment/locale/en-au.js"></script>
+<script src="_assets_/plugins/social-feed/js/jquery.socialfeed.js"></script>
+<script src="_assets_/js/jquery.mb.YTPlayer.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
